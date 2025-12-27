8ad408de68ca73e5abd8a1ac1e9a9228ee8630e6
Felipe <adm.otmizards@gmail.com>
2025-12-27 16:39:53 +0000

    init site03

---
+++ src/config/site.js
@@ -0,0 +1,7 @@
+export const SITE = {
+  name: "360Wins PT",
+  description:
+    "Conteúdos educativos, guias e boas práticas sobre jogos online. Informação clara, sem promessas.",
+  url: "https://site01-4im.pages.dev",
+  telegram: "https://t.me/+cIQNof1Xess1NDE8",
+};
