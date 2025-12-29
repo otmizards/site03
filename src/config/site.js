874ae839f3969ae40ac6047a0c44e2f6b50981f2
Felipe <adm.otmizards@gmail.com>
2025-12-29 16:53:55 +0000

    chore: update siteUrl to final domain

--- src/config/site.js
+++ src/config/site.js
@@ -1,12 +1,12 @@
 export const siteConfig = {
   siteName: "Guias de Casino Online em Portugal",
   siteDescription: "Guias claros e atualizados sobre casino online em Portugal. Explicações simples, boas práticas e acesso a comunidades no Telegram com novidades.",
-  siteUrl: "https://site02.pages.dev",
+  siteUrl: "https://site03.360winspt.com",
   telegramUrl: "https://t.me/+cIQNof1Xess1NDE8",
 
   name: "Guias de Casino Online em Portugal",
   description: "Guias claros e atualizados sobre casino online em Portugal. Explicações simples, boas práticas e acesso a comunidades no Telegram com novidades.",
-  url: "https://site02.pages.dev",
+  url: "https://site03.360winspt.com",
   telegram: "https://t.me/+cIQNof1Xess1NDE8",
 };
 
